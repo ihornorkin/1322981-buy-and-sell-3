8a922cd94959d7eedabcaab0c43f673343f66bb2
ihornorkin <ihornorkin@gmail.com>
2020-02-23 18:39:10 +0200

    Write readme

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -13,6 +13,7 @@ module.exports = {
     Команды:
     --version:            выводит номер версии
     --help:               печатает этот текст
-    --generate <count>    формирует файл mocks.json`));
+    --generate <count>    формирует файл mocks.json
+    --server <port>       создает веб-сервер на заданом порте`));
   }
 };
